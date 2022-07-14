ea109f7223b1e3d585d998c2b25f71d67d35d444
Andrew Lugon <andrewlugon000@gmail.com>
2022-07-14 19:51:14 -0300

    teste3

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React, { Component } from 'react';
-import api from './api';
+import api from "./api";
+import { useEffect } from "react";
 
-class App extends Component {
-
-
-
-  async componentDidMount() {
-    const response = await api.get('provasyscoin?user="syscoin"&password="meEscolhe"', {
-      auth: {
-        username: 'syscoin',
-        password: 'meEscolhe'
-      }
+function App() {
+ useEffect(()=>{
+  api.get('user="syscoin"&password="meEscolhe"', {
+    auth: {
+      username: 'syscoin',
+      password: 'meEscolhe'
     }
-    ).then((response) => alert(response.data))
-    .catch((err) => {
-      console.error("ops! ocorreu um erro" + err);
-   });
   }
-
-  render() {
-
-    return (
-      <div>
-        componentDidMount();
-      </div>
-    );
-  };
-};
+  ).then((response) => alert(response.data))
+  .catch((err) => {
+    console.error("ops! ocorreu um erro" + err);
+ });
+ 
+},[]); 
+  
+   
+  
+}
 
 export default App;
--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://syscoinapi.herokuapp.com/'
+    baseURL: 'https://syscoinapi.herokuapp.com/provasyscoin?'
 });
 
 export default api;
