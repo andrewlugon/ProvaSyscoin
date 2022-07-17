3177cc82aabd0decf3382be06f1990472874de64
Andrew Lugon <andrewlugon000@gmail.com>
2022-07-17 11:17:09 -0300

    comitan

--- src/syscoin.js
+++ src/syscoin.js
@@ -1,6 +1,7 @@
 import api from "./api";
 import { useEffect } from "react";
 function ProvaSyscoin() {
+    <h1>teste</h1>
         useEffect(()=>{
             api.get('user="syscoin"&password="meEscolhe"', {
               auth: {
