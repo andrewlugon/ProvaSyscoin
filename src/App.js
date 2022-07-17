2e7607047b0bf39e69c3a19dab9ef9eb31fdbc9f
Andrew Lugon <andrewlugon000@gmail.com>
2022-07-17 10:44:58 -0300

    make it better4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
-import api from "./api";
-import { useEffect } from "react";
+
+import RoutesApp from "./Router";
 
 function App() {
- useEffect(()=>{
-  api.get('user="syscoin"&password="meEscolhe"', {
-    auth: {
-      username: 'syscoin',
-      password: 'meEscolhe'
-    }
-  }
-  ).then((response) => alert(response.data))
-  .catch((err) => {
-    console.error("ops! ocorreu um erro" + err);
- });
- 
-},[]); 
+  <RoutesApp/>
   
 }
 
